af9f51fbe7046df47f0846b61a2c221e0c7e8174
Travis Thieman <travis.thieman@gmail.com>
2015-04-30 22:20:12 -0400

    Derp

--- daemon/list.go
+++ daemon/list.go
@@ -134,16 +134,15 @@ func (daemon *Daemon) Containers(config *ContainersConfig) ([]*types.Container, 
 			Names: names[container.ID],
 		}
 
-		// img, err := daemon.Repositories().LookupImage(container.Config.Image)
-		// if err != nil {
-		// 	return err
-		// }
-		// if container.ImageID == img.ID {
-		// 	newC.Image = container.Config.Image
-		// } else {
-		// 	newC.Image = container.ImageID
-		// }
-		newC.Image = container.Config.Image
+		img, err := daemon.Repositories().LookupImage(container.Config.Image)
+		if err != nil {
+			return err
+		}
+		if container.ImageID == img.ID {
+			newC.Image = container.Config.Image
+		} else {
+			newC.Image = container.ImageID
+		}
 
 		if len(container.Args) > 0 {
 			args := []string{}
